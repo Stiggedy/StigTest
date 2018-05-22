f6246deeda6bd8143ef6ec4797b43e14ab778001
Robert Pietsch <robp.de@googlemail.com>
2018-05-22 23:10:56 +0200

    added new function

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,16 @@ stig_add2 <- function(x) {
   x <- x + 2
   return(x)
 }
+
+#' adding 4 to a value
+#'
+#' @param x integer
+#'
+#' @return x increased by 4
+#' @export
+#'
+#' @examples stig_add4(8) #12
+stig_add4 <- function(x) {
+  x <- x + 4
+  return(x)
+}
