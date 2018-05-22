cc6c5c1693577e08f0d4e2e8e62a0c4f2db2ea6f
Robert Pietsch <robp.de@googlemail.com>
2018-05-22 23:17:45 +0200

    adding 6

--- R/functions.R
+++ R/functions.R
@@ -23,3 +23,16 @@ stig_add4 <- function(x) {
   x <- x + 4
   return(x)
 }
+
+#' adding 6 to a value
+#'
+#' @param x integer
+#'
+#' @return x increased by 4
+#' @export
+#'
+#' @examples stig_add6(8) #14
+stig_add6 <- function(x) {
+  x <- x + 6
+  return(x)
+}
